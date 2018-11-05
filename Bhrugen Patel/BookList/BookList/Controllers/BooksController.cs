b18c43088e79c6cebf6b311c6b98e2944c5152fa
kgalaszewski <kgalaszewski95@gmail.com>
2018-11-05 16:05:56 +0000

    Finished BookList Project

---
+++ Bhrugen Patel/BookList/BookList/Controllers/BooksController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Books.ToList());
+        }
+
+        //Get: Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if(id == null)            
+                return NotFound();
+            
+            Book book = await _db.Books.SingleOrDefaultAsync(b=>b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);            
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Book book = await _db.Books.SingleOrDefaultAsync(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Book book)
+        {
+            if (id != book.Id)
+                return NotFound();
+
+            if(ModelState.IsValid)
+            {
+                _db.Update(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")] // jak tak zrobimy to metoda(akcja) moze sie nazywac dowolnie
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RemoveBook(int id)
+        {
+            Book book = await _db.Books.SingleOrDefaultAsync(b => b.Id == id);
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            Book book = await _db.Books.SingleOrDefaultAsync(b => b.Id == id);
+
+            if (book == null)
+                return NotFound();
+
+            return View(book);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _db.Dispose();
+            }
+        }
+    }
+}
